c55788ddfd37fc73a8d469666b7d47b4a5c4f157
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-11-18 14:11:21 +0600

    welcome File Updated

--- L11 CRUD Operation/resources/views/welcome.blade.php
+++ L11 CRUD Operation/resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 flex justify-center items-center h-screen">
-
     <div class="bg-blue-500 text-white font-bold p-8 rounded-lg shadow-lg">
         <h1 class="text-3xl">Tailwind CSS is Working!</h1>
         <p class="mt-4 text-lg">
