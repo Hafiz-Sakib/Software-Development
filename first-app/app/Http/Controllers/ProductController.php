ab00847c3e0b7f22f0ee2b5edb21ac000272d434
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-11-18 10:16:41 +0600

    update

--- first-app/app/Http/Controllers/ProductController.php
+++ first-app/app/Http/Controllers/ProductController.php
@@ -3,29 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    // This Method Will Show products page
+    // This method will show products page
     public function index()
     {
-        return view('products.list');
+        $products = Product::orderBy('created_at', 'DESC')->get();
+
+        return view('products.list', [
+            'products' => $products
+        ]);
     }
-    //This Method Will Show Create Product page
+
+    // This method will show create product page
     public function create()
     {
         return view('products.create');
     }
-    //This Method Will store a product in db
+
+    // This method will store a product in db
     public function store(Request $request)
     {
         $rules = [
             'name' => 'required|min:5',
             'sku' => 'required|min:3',
-            'price' => 'required|numeric',
+            'price' => 'required|numeric'
         ];
+
+        if ($request->image != "") {
+            $rules['image'] = 'image';
+        }
 
         $validator = Validator::make($request->all(), $rules);
 
@@ -33,7 +44,7 @@ class ProductController extends Controller
             return redirect()->route('products.create')->withInput()->withErrors($validator);
         }
 
-        // Here We Will Add Products into DB
+        // here we will insert product in db
         $product = new Product();
         $product->name = $request->name;
         $product->sku = $request->sku;
@@ -41,12 +52,93 @@ class ProductController extends Controller
         $product->description = $request->description;
         $product->save();
 
-        return redirect()->route('products.index')->with('success', 'Product Added Successfully.');
+        if ($request->image != "") {
+            // here we will store image
+            $image = $request->image;
+            $ext = $image->getClientOriginalExtension();
+            $imageName = time() . '.' . $ext; // Unique image name
+
+            // Save image to products directory
+            $image->move(public_path('uploads/products'), $imageName);
+
+            // Save image name in database
+            $product->image = $imageName;
+            $product->save();
+        }
+
+        return redirect()->route('products.index')->with('success', 'Product added successfully.');
     }
-    //This Method Will show edit Product page
-    public function edit() {}
-    //This Method Will update a product
-    public function update() {}
-    //This Method Will delete a product
-    public function delete() {}
+
+    // This method will show edit product page
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.edit', [
+            'product' => $product
+        ]);
+    }
+
+    // This method will update a product
+    public function update($id, Request $request)
+    {
+
+        $product = Product::findOrFail($id);
+
+        $rules = [
+            'name' => 'required|min:5',
+            'sku' => 'required|min:3',
+            'price' => 'required|numeric'
+        ];
+
+        if ($request->image != "") {
+            $rules['image'] = 'image';
+        }
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->route('products.edit', $product->id)->withInput()->withErrors($validator);
+        }
+
+        // here we will update product
+        $product->name = $request->name;
+        $product->sku = $request->sku;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->save();
+
+        if ($request->image != "") {
+
+            // delete old image
+            File::delete(public_path('uploads/products/' . $product->image));
+
+            // here we will store image
+            $image = $request->image;
+            $ext = $image->getClientOriginalExtension();
+            $imageName = time() . '.' . $ext; // Unique image name
+
+            // Save image to products directory
+            $image->move(public_path('uploads/products'), $imageName);
+
+            // Save image name in database
+            $product->image = $imageName;
+            $product->save();
+        }
+
+        return redirect()->route('products.index')->with('success', 'Product updated successfully.');
+    }
+
+    // This method will delete a product
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+
+        // delete image
+        File::delete(public_path('uploads/products/' . $product->image));
+
+        // delete product from database
+        $product->delete();
+
+        return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
+    }
 }
--- first-app/resources/views/products/list.blade.php
+++ first-app/resources/views/products/list.blade.php
@@ -1,12 +1,87 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World</h1>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simple Laravel 11 CRUD</title>
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <div class="bg-gray-800 py-3">
+        <h3 class="text-white text-center">Simple Laravel 11 CRUD</h3>
+    </div>
+    <div class="container mx-auto px-4">
+        <div class="flex justify-end mt-4">
+            <a href="{{ route('products.create') }}" class="px-4 py-2 bg-gray-800 text-white rounded">Create</a>
+        </div>
+        <div class="flex justify-center mt-4">
+            @if (Session::has('success'))
+            <div class="w-full max-w-3xl">
+                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded">
+                    {{ Session::get('success') }}
+                </div>
+            </div>
+            @endif
+        </div>
+        <div class="flex justify-center mt-4">
+            <div class="w-full max-w-5xl bg-white shadow-lg rounded">
+                <div class="bg-gray-800 px-4 py-3 rounded-t">
+                    <h3 class="text-white">Products</h3>
+                </div>
+                <div class="px-4 py-4">
+                    <table class="w-full border-collapse">
+                        <thead>
+                            <tr class="bg-gray-800 text-white">
+                                <th class="border px-4 py-2">ID</th>
+                                <th class="border px-4 py-2"></th>
+                                <th class="border px-4 py-2">Name</th>
+                                <th class="border px-4 py-2">SKU</th>
+                                <th class="border px-4 py-2">Price</th>
+                                <th class="border px-4 py-2">Created At</th>
+                                <th class="border px-4 py-2">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if ($products->isNotEmpty())
+                            @foreach ($products as $product)
+                            <tr class="border hover:bg-gray-100">
+                                <td class="border px-4 py-2">{{ $product->id }}</td>
+                                <td class="border px-4 py-2">
+                                    @if ($product->image != "")
+                                    <img width="50" src="{{ asset('uploads/products/'.$product->image) }}" alt="Product Image">
+                                    @endif
+                                </td>
+                                <td class="border px-4 py-2">{{ $product->name }}</td>
+                                <td class="border px-4 py-2">{{ $product->sku }}</td>
+                                <td class="border px-4 py-2">${{ $product->price }}</td>
+                                <td class="border px-4 py-2">{{ \Carbon\Carbon::parse($product->created_at)->format('d M, Y') }}</td>
+                                <td class="border px-4 py-2">
+                                    <a href="{{ route('products.edit', $product->id) }}" class="px-3 py-1 bg-blue-600 text-white rounded">Edit</a>
+                                    <a href="#" onclick="deleteProduct({{ $product->id }});" class="px-3 py-1 bg-red-600 text-white rounded">Delete</a>
+                                    <form id="delete-product-form-{{ $product->id }}" action="{{ route('products.destroy', $product->id) }}" method="post" class="hidden">
+                                        @csrf
+                                        @method('delete')
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                            @else
+                            <tr>
+                                <td colspan="7" class="text-center py-4">No Products Found</td>
+                            </tr>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function deleteProduct(id) {
+            if (confirm("Are you sure you want to delete this product?")) {
+                document.getElementById("delete-product-form-" + id).submit();
+            }
+        }
+    </script>
+  </body>
 </html>
