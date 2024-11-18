5ecd52d150bb16ced3677d5699b365bf740fc1b4
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-11-18 09:35:05 +0600

    first commit

---
+++ first-app/app/Http/Controllers/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // This Method Will Show products page
+    public function index() {}
+    //This Method Will Show Create Product page
+    public function create()
+    {
+        return view('products.create');
+    }
+    //This Method Will store a product in db
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|min:5',
+            'sku' => 'required|min:3',
+            'price' => 'required|numeric',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->route('products.create')->withInput()->withErrors($validator);
+        }
+
+        // Here We Will Add Products into DB
+        $product = new Product();
+        $product->name = $request->name;
+        $product->sku = $request->sku;
+        $product->price = $request->price;
+        $product->descroption = $request->descroption;
+        $product->save();
+
+        return redirect();
+    }
+    //This Method Will show edit Product page
+    public function edit() {}
+    //This Method Will update a product
+    public function update() {}
+    //This Method Will delete a product
+    public function delete() {}
+}
