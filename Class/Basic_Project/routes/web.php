f608feeaa27e85c5a02a68261ea4c7bc9cec7a67
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-11-24 20:21:33 +0600

    Enroll Message Completed

--- Class/Basic_Project/routes/web.php
+++ Class/Basic_Project/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
